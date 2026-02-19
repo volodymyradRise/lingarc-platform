2ed425a367753494cca7b865552d2be5f3135cbb
volodymyradRise <volodymyr.hridin@gmail.com>
2026-02-19 17:27:42 +0100

    Create index.ts

---
+++ lib/i18n/dictionaries/index.ts
@@ -0,0 +1,13 @@
+// lib/i18n/dictionaries/index.ts
+import { en } from './en';
+import { uk } from './uk';
+import { ru } from './ru';
+import { es } from './es';
+import type { Translations } from '../types';
+
+export const dictionaries: Translations = {
+  en,
+  uk,
+  ru,
+  es
+};
